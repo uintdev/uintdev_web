d3df8e5c5f9d28400a6518314c71df6af85c64ea
uintdev <core@uint.dev>
2026-05-04 00:08:03 +0100

    Rename section

--- fetch_blog.ts
+++ fetch_blog.ts
@@ -8,7 +8,7 @@ import * as cheerio from "cheerio";
 const BLOG_URL: string = "https://blog.uint.dev";
 const SELECTOR: string = ".listing .card";
 const POST_LIMIT: number = 5;
-const BLOG_TITLE: string = "Blog";
+const BLOG_TITLE: string = "Recent Posts";
 const BLOG_DESCRIPTION: string = `See all <a href="${BLOG_URL}">here</a>.`;
 
 // Data structures
